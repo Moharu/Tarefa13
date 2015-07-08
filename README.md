59acad69ba714d310052e18a2e4f197fb123af5f
Joao Telo <joaotelo.nh@hotmail.com>
2015-07-08 02:54:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+To build a standalone version:
+
+* Have WinRar installed.
+* Just select all files from your build and add to Archive marking 'create SFX archive'.
+* Go to Advanced -> SFX Options..., and in Setup write up Tarefa13.exe in the Run after Extraction textarea
+* In Mode Tab choose Unpack to temporary folder and then select Hide All.
---
+++ app/mainController.js
@@ -0,0 +1,32 @@
+app
+	.controller('mainController',
+		function($scope){
+			$scope.showCalc = false;
+			$scope.calculate = function (){
+				var date = moment($scope.dt);
+				if(!date.isValid()){
+					$scope.invalidDate = true;
+					return;
+				}
+				$scope.invalidDate = false;
+				//Dia da semana do nascimento
+				$scope.birthWeekDay = date.format('dddd');
+				//Ano bissexto?
+				$scope.leapYear = date.isLeapYear()? 'Sim' : 'Não';
+
+				//Prox aniversário
+				nextBirthday = moment(date);
+				nextBirthday.year(moment().year());
+				if(nextBirthday.isBefore(moment(), 'day')){
+					nextBirthday.add(1, 'year');
+				}
+				//Próximo aniversário em quantos dias?
+				$scope.nextBirthdayIn = nextBirthday.diff(moment().startOf('day'), 'days');
+				//Dia da semana prox aniversário
+				$scope.nextBirthdayWeekDay = nextBirthday.format('dddd');
+				$scope.showCalc = true;
+
+				//easteregg trô
+				$scope.atropelando = date.isSame('1997-01-01', 'year');
+			};
+	});
